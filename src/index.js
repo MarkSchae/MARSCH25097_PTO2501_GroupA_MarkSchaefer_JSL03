c30811ca252926ff3cff49f27fe08f9f47b73480
Mark <mcschaefer26@gmail.com>
2025-06-19 22:09:51 +0200

    Added a function (click) to run the code to add tasks.

--- src/index.js
+++ src/index.js
@@ -6,29 +6,34 @@ const tasks = [];
 let uniqueId = 0; // This will probably need to be stored and fetched/updated based on the last stored number, assign the variable to the local storage
 // Button to add new tasks here to run this code as a function, makes it easier to test the codes functionality
 const addTaskBtn = document.querySelector('#btn-add-task');
-// Promt the user to enter title, description, and status for 2 seperate tasks
-// Loop to prompt the user for 3 task entries
-for(let i = 0; i < 3; i++) {
-    // Store the user answers in variables
-    const title = prompt(`Please enter the title of the task ${i + 1}`);
-    const description = prompt('Please enter the description of the task');
-    let taskStatus = prompt('Please enter the status of the task. Valid status: todo, done, doing').toLowerCase(); // Convert status inputs to lowercase
-    // Validate the status inputs to only accept todo, doing, done, repeat the prompt until a valid input is entered
-    // Run the prompt until the entry is valid 
-    while (taskStatus !== 'done' && taskStatus !== 'doing' && taskStatus !== 'todo') {
-        alert('Sorry you did not enter a valid status for the task, please enter todo, done, or doing');
-        taskStatus = prompt('Please enter the status of the task. Valid status: todo, done, doing').toLowerCase();
+addTaskBtn.addEventListener('click', clickToAddTask);
+// Encase the code in a function that can be run on a button click
+function clickToAddTask() {
+    // Prompt the user to enter title, description, and status for 3 seperate tasks
+    // Loop to prompt the user for 3 task entries, advance to just adding one at a time in future
+    for(let i = 0; i < 3; i++) {
+        // Store the user answers in variables
+        const title = prompt(`Please enter the title of the task ${i + 1}`);
+        const description = prompt('Please enter the description of the task');
+        let taskStatus = prompt('Please enter the status of the task. Valid status: todo, done, doing').toLowerCase(); // Convert status inputs to lowercase
+        // Validate the status inputs to only accept todo, doing, done, repeat the prompt until a valid input is entered
+        // Run the prompt until the entry is valid 
+        while (taskStatus !== 'done' && taskStatus !== 'doing' && taskStatus !== 'todo') {
+            alert('Sorry you did not enter a valid status for the task, please enter todo, done, or doing');
+            taskStatus = prompt('Please enter the status of the task. Valid status: todo, done, doing').toLowerCase();
+        }
+        //Increment id for each new task
+        uniqueId++;
+        // Create new object and push to the array
+        tasks.push({
+            uniqueId: uniqueId,
+            title: title,
+            description: description,
+            status: taskStatus
+        });
     }
-    //Increment id for each new task
-    uniqueId++;
-    // Create new object and push to the array
-    tasks.push({
-        uniqueId: uniqueId,
-        title: title,
-        description: description,
-        status: taskStatus
-    });
-}
+};
+
 // Display the title and status of completed 'done' tasks in the console
 let doneTasks = false;
 for(let i = 0; i < tasks.length; i++) {
